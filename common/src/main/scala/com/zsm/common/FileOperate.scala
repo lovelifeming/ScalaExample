1b8b6e8b6e19f5fe3cfa8cfe454f4b015713c535
zengsm <453752303@qq.com>
2019-01-01 23:39:48 +0800

    Optimize code structure and remove invalid code.

--- common/src/main/scala/com/zsm/common/FileOperate.scala
+++ common/src/main/scala/com/zsm/common/FileOperate.scala
@@ -15,21 +15,18 @@ object FileOperate {
   def main(args: Array[String]): Unit = {
 
     val writer = new PrintWriter(new File("C:\\Users\\Administrator\\Desktop\\移动\\result.txt"))
-    val path:String="C:\\Users\\Administrator\\Desktop\\移动\\S1U-103-2018111705-109-0.txt";
+    val path: String = "C:\\Users\\Administrator\\Desktop\\移动\\S1U-103-2018111705-109-0.txt";
 
-    val data=Source.fromFile(path);
-    val lines=data.getLines();
-    while (lines.hasNext)
-    {
-      val sp=lines.next()
+    val data = Source.fromFile(path);
+    val lines = data.getLines();
+    while (lines.hasNext) {
+      val sp = lines.next()
       val str = sp.split("\\|")
       val val1 = str(72)
-      val val2=str(73)
+      val val2 = str(73)
       //        if(val1)
-      writer.append(val1+"|"+val2+"\n")
+      writer.append(val1 + "|" + val2 + "\n")
     }
-
-
     writer.close()
     println("end")
   }
--- common/src/main/scala/com/zsm/common/Main.scala
+++ common/src/main/scala/com/zsm/common/Main.scala
@@ -10,9 +10,5 @@ object Main {
 
   def main(args: Array[String]): Unit = {
 
-    print(DateUtils.TIME_FORMAT_SHORT.toPattern)
-    val bool = DateUtils.before("2018-12-1","2018-12-2",DateUtils.TIME_FORMAT_SHORT)
-    print(bool)
-
   }
 }
